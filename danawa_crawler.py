837b3595e1a2077b290788f10074dce8619b5c3f
SSun <90237785+SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSun@users.noreply.github.com>
2025-01-16 11:02:12 +0900

    Update danawa_crawler.py

--- danawa_crawler.py
+++ danawa_crawler.py
@@ -82,6 +82,7 @@ class DanawaCrawler:
         self.chrome_option.add_argument('--start-maximized')
         self.chrome_option.add_argument('--disable-gpu')
         self.chrome_option.add_argument('lang=ko=KR')
+        self.chrome_option.binary_location = "C:\hostedtoolcache\windows\setup-chrome\chromedriver\beta\x64\chromedriver.exe"
         if __name__ == '__main__':
             pool = Pool()
             pool.map(self.CrawlingCategory, self.crawlingCategory)
